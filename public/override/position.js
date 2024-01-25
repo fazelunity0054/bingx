bd3f05a39d4662220d6aa0998fc7251a86d928dd
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-01-25 15:44:18 +0330

    update

--- public/override/position.js
+++ public/override/position.js
@@ -193,7 +193,7 @@ function handleRefreshPositions() {
                                     <p class="label dotted" data-v-db2fc607="">Est.
                                         Liq. Price
                                     </p>
-                                    <p class="value up" data-key="liq" data-v-db2fc607="">${position.liq ? echoN(position.liq, currency.priceDigitNum):"Calculating"}</p>
+                                    <p class="value up" data-key="liq" data-v-db2fc607="">${typeof position.liq !== 'undefined' ? echoN(position.liq, currency.priceDigitNum):"Calculating"}</p>
                                 </div>
                                 <!---->
                             </div>
