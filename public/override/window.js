fc72cf8f777f823e69df8b6514b6c0af8b3a47d3
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-01-25 18:10:30 +0330

    update

--- public/override/window.js
+++ public/override/window.js
@@ -414,7 +414,7 @@ window.handleLiquidCalculation = (key, position, callback) => {
                 let target = +position?.balance ?? +(+variables.balance).toFixed(5);
                 const mode = iframeDoc.querySelector("#__layout > div > div > div > section > div.calculator-container > div > div.inner > div:nth-child(6) > div > div > div.label.long").innerText;
                 if (mode.includes("Position")) {
-                    target = position.forceMargin ?? Object.values(positions).filter(p => p.asset === position.asset).reduce((total, p) => total + p.margin, 0);
+                    target = position.forceMargin ?? Object.entries(positions).filter(([k, p]) => k !== key && p.asset === position.asset).reduce((total, p) => total + p.margin, 0);
                 }
                 const bIn = iframeDoc.querySelector("#__layout > div > div > div > section > div.calculator-container > div > div.inner > div:nth-child(6) > div > div > div.input > input[type=number]");
                 input(bIn)
