62d41db34cbf5201b330354cec5649423400bf14
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-01-31 20:33:52 +0330

    ACEdit_override/dark

--- public/override/dark.js
+++ public/override/dark.js
@@ -2,3 +2,27 @@ const st = document.createElement("link");
 st.href = "/override/dark.css";
 st.rel = "stylesheet";
 document.head.append(st);
+
+
+window.onload = function() {
+    const image = document.querySelector("img");
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext('2d');
+    image.parentNode.insertBefore(image, canvas);
+
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    ctx.drawImage(image, 0, 0);
+
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+        if (data[i] === 255 && data[i + 1] === 255 && data[i + 2] === 255) { // if white
+            data[i + 3] = 0; // set alpha to 0
+        }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+};
