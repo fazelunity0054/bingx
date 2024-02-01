e5c2a1ace1088724337fa3a4036ba3a43988383d
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-02-01 04:43:17 +0330

    ACEdit_public/index

--- public/override/position.js
+++ public/override/position.js
@@ -124,7 +124,7 @@ function handleRefreshPositions() {
         let positionSize = (position.margin * position.leverage) / position.openedPrice;
 
         if (typeof position.liq === 'undefined') handleLiquidCalculation(key, position, (number) => {
-            if (window.positions[key] && !window.positions[key].liq) window.positions[key] = {
+            if (window.positions[key] && !window.positions[key].liq && number) window.positions[key] = {
                 ...positions[key],
                 liq: number
             }
