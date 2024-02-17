8e9b413eed7e98c884a75aa3d218f3682fa2c03d
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-02-18 03:20:38 +0330

    ACEdit_public/select

--- public/override/select.js
+++ public/override/select.js
@@ -71,7 +71,6 @@ function onMessage(object) {
             socket.send(`{"dataType":"market.contracts","id": "UPDATE-LIST","reqType":"sub"}`)
             break;
         case "market.contracts":
-            console.log(object)
             /**
              * @type {any[]}
              */
