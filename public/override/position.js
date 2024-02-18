1f53b1719a63b27701f531b23152d1e1cb72c876
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-02-18 04:19:30 +0330

    ACEdit_override/position

--- public/override/position.js
+++ public/override/position.js
@@ -329,7 +329,7 @@ function handleRefreshPositions() {
                 if (autoPrompt) {
                     const iframe = document.createElement("iframe");
                     iframe.src = "/export.html?auto";
-                    iframe.style.opacity = 0;
+                    iframe.style.display = "none";
                     document.body.append(iframe);
                 } else {
                     window.location.href="/export.html";
