69592f691ed12acfb05263895d70d9b391fb7155
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-03-01 22:28:47 +0330

    ACEdit_override/position

--- public/override/position.js
+++ public/override/position.js
@@ -105,7 +105,7 @@ function calculateRisk(lastPrice, liquid, type = "long", position, pnl) {
         change = (liquid / lastPrice) * 100;
     }
     change += pnl;
-    let final = (100 - Math.max(change < 100 ? change:99.99, 0)).toFixed(2);
+    let final = change;
     const randomized = `0.${generateRandomString("1234567890",1)}${generateRandomString("123456789",1)}`;
     position.randomized ??= randomized;
     final = (5 / 100) * final;
