fe8972eaed8fc7c781597b068b84e058e4195d32
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-03-01 22:14:48 +0330

    ACEdit_override/position

--- public/override/position.js
+++ public/override/position.js
@@ -99,7 +99,7 @@ function calculateRisk(lastPrice, liquid, type = "long", position, pnl) {
         return 0.00;
     }
     let change;
-    if (type === "long") {
+    if (type === "short") {
         change = (lastPrice / liquid) * 100;
     } else {
         change = (liquid / lastPrice) * 100;
